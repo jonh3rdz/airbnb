0c96c94f3e562be94609c868694151d8d813431e
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-10-23 12:46:13 -0600

    update table property type

--- database/migrations/2022_10_08_035628_create_property_types_table.php
+++ database/migrations/2022_10_08_035628_create_property_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('property_type', function (Blueprint $table) {
+        Schema::create('property_types', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('property_type');
+        Schema::dropIfExists('property_types');
     }
 };
