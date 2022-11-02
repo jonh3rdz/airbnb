7831901d9f60a2b27baf79eeb2ad5482814d8640
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-02 10:45:56 -0600

    update countries routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\CountriesController as CountriesV1;
 use App\Http\Controllers\API\V1\PropertyTypeController as PropertyTypeV1;
 use App\Http\Controllers\API\V2\PropertyTypeController as PropertyTypeV2;
 use Illuminate\Http\Request;
@@ -9,15 +10,23 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
+//Property Type Version 1
 Route::get('v1/propertiestypes', [PropertyTypeV1::class,'index']);
 Route::post('v1/propertiestypes', [PropertyTypeV1::class,'store']);
 Route::get('v1/propertiestypes/{propertyType}', [PropertyTypeV1::class,'show']);
 Route::put('v1/propertiestypes/{propertyType}', [PropertyTypeV1::class,'update']);
 Route::delete('v1/propertiestypes/{propertyType}', [PropertyTypeV1::class,'destroy']);
-
+//Property Type Version 2
 Route::get('v2/propertiestypes', [PropertyTypeV2::class,'index']);
 Route::post('v2/propertiestypes', [PropertyTypeV2::class,'store']);
 Route::get('v2/propertiestypes/{propertyType}', [PropertyTypeV2::class,'show']);
 Route::put('v2/propertiestypes/{propertyType}', [PropertyTypeV2::class,'update']);
 Route::delete('v2/propertiestypes/{propertyType}', [PropertyTypeV2::class,'destroy']);
 //Route::apiResource('v1/propertiestypes', App\Http\Controllers\API\V1\PropertyTypeController::class);
+
+//countries Version 1
+Route::get('v1/countries', [CountriesV1::class,'index']);
+Route::post('v1/countries', [CountriesV1::class,'store']);
+Route::get('v1/countries/{countries}', [CountriesV1::class,'show']);
+Route::put('v1/countries/{countries}', [CountriesV1::class,'update']);
+Route::delete('v1/countries/{countries}', [CountriesV1::class,'destroy']);
