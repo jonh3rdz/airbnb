64af222c5533caeeeb9a47a040cc6b763fc5580b
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-02 17:53:52 -0600

    Update Room Type

--- app/Http/Controllers/API/V1/RoomTypeController.php
+++ app/Http/Controllers/API/V1/RoomTypeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\RoomType\StoreRoomTypeRequest;
+use App\Http\Requests\API\V1\RoomType\UpdateRoomTypeRequest;
 use App\Http\Resources\API\V1\RoomType\RoomTypeCollection;
 use App\Http\Resources\API\V1\RoomType\RoomTypeResource;
 use App\Models\API\V1\RoomType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class RoomTypeController extends Controller
 {
@@ -42,13 +44,47 @@ class RoomTypeController extends Controller
         return response()->json(new RoomTypeResource($RoomTypeId),200);
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateRoomTypeRequest $request, $RoomTypeId)
     {
-        //
+        $RoomType = RoomType::findOrFail($RoomTypeId);
+        if ($request->hasFile('icon_image')){
+            if (File::exists("storage/RoomType/".$RoomType->icon_image)) {
+                File::delete("storage/RoomType/".$RoomType->icon_image);
+            }
+            $image = $request->file('icon_image');
+            $ext = $image->extension();
+            $file = time().'.'.$ext;
+            $image->storeAs('public/RoomType', $file);
+            $RoomType->icon_image = $file;
+
+            $request['icon_image'] = $RoomType->icon_image;
+        }
+        
+        $RoomType->update([$request->all(),
+        "icon_image"=>$RoomType->icon_image,]);
+        
+        return response()->json([
+            'res' => true, //Retorna una respuesta
+            'data' => $RoomType, //retorna toda la data
+            'msg' => 'Actualizado correctamente' //Retorna un mensaje
+        ],200);
     }
 
-    public function destroy($id)
+    public function destroy(RoomType $request, $RoomTypeId)
     {
-        //
+        $RoomType = RoomType::findOrFail($RoomTypeId);
+        
+        if (File::exists("storage/RoomType/".$RoomType->icon_image)) {
+            File::delete("storage/RoomType/".$RoomType->icon_image);
+        }
+
+        $RoomType->delete();
+        
+        return response()->json([
+            'res' => true, //Retorna una respuesta
+            'data' => $RoomType, //retorna toda la data
+            'message' => 'Eliminado correctamente'
+        ],200);
+        //204 No Content
     }
 }
