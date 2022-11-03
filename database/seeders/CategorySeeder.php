89eaf9ddc72d67b157a337228e4b3f4b9820c179
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-03 11:34:48 -0600

    Category Resource

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'title' => 'Un alojamiento entero',
+                'description' => 'Un alojamiento entero',
+                'icon_image' => 'tmp1',
+                'status' => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'title' => 'Una habitacion privada',
+                'description' => 'Una habitacion privada',
+                'icon_image' => 'tmp2',
+                'status' => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'title' => 'Una habitacion compartida',
+                'description' => 'Una habitacion compartida',
+                'icon_image' => 'tmp3',
+                'status' => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
