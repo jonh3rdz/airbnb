468ac5567f76a71e1bb08f586a06e7e79551f1ff
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-02 19:49:52 -0600

    City Resource

---
+++ app/Http/Resources/API/V1/City/CityCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\API\V1\City;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CityCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
