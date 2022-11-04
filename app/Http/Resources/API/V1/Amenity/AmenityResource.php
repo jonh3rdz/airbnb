e14691a230f084194606e380d372a262dadfc6eb
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-04 07:56:36 -0600

    Amenity Resource

---
+++ app/Http/Resources/API/V1/Amenity/AmenityResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\API\V1\Amenity;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AmenityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
---
+++ app/Models/API/V1/Amenity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\API\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Amenity extends Model
+{
+    use HasFactory;
+}
