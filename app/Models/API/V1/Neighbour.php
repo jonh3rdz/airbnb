4ae4cd35e62509299284d4efedad760d5132536d
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-04 06:03:08 -0600

    Neighbour Resource

---
+++ app/Models/API/V1/Neighbour.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\API\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Neighbour extends Model
+{
+    use HasFactory;
+}
