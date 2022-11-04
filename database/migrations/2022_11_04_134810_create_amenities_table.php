4ddc8523ba9082719f33b6bbd69487e40cfa1144
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-04 14:03:19 -0600

    Update Amenity

--- database/migrations/2022_11_04_134810_create_amenities_table.php
+++ database/migrations/2022_11_04_134810_create_amenities_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('icon_image')->nullable();
             $table->string('status')->nullable()->default(1);
+            
             $table->timestamps();
         });
     }
