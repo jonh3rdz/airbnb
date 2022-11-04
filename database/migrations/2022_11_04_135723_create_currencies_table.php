3bc13ce03e608c1553f572fd9830f88aa5a96cad
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2022-11-04 14:56:23 -0600

    Update Currencies Table

--- database/migrations/2022_11_04_135723_create_currencies_table.php
+++ database/migrations/2022_11_04_135723_create_currencies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('currencies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('icon_image')->nullable();
+            $table->string('status')->nullable()->default(1);
             $table->timestamps();
         });
     }
